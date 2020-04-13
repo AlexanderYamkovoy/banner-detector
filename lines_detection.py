327e54473198c0877c1b18aee953387b5d6f2507
AlexanderYamkovoy <yamkovoy.a03@gmail.com>
2020-04-13 17:36:43 +0300

    added fragments detection method

--- lines_detection.py
+++ lines_detection.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import math
 import pandas as pd
+import time
 
 
 def detect_lines(image):
@@ -95,7 +96,7 @@ def detect_shapes(image):
 
     kernel = 5
     min_area_threshold = 2000
-    max_area_threshold = 50000
+    max_area_threshold = 80000
     perimeter_threshold = 0.035
 
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -112,6 +113,7 @@ def detect_shapes(image):
                  or cv.contourArea(contours[i]) > max_area_threshold]
     contours = [i for j, i in enumerate(contours) if j not in drop_list]
 
+    frames_contours = []
     for cnt in contours:
         epsilon = perimeter_threshold * cv.arcLength(cnt, True)
         approx = cv.approxPolyDP(cnt, epsilon, True)
@@ -119,33 +121,149 @@ def detect_shapes(image):
         corners_count = len(approx)
 
         if convexity and corners_count == 4:
+            frames_contours.append(approx.tolist())
             cv.drawContours(image, [approx], -1, (0, 255, 0), 2)
+    return frames_contours
 
 
-def build_model(video, img_name='Set name', results_folder='Set path'):
+def data_collection(image):
+    kernel = 5
+    min_area_threshold = 2000
+    max_area_threshold = 80000
+    perimeter_threshold = 0.035
+
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    blur_gray = cv.GaussianBlur(gray, (kernel, kernel), 0)
+
+    _, th = cv.threshold(blur_gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+    _, contours, __ = cv.findContours(th, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    drop_list = [i for i in range(len(contours))
+                 if cv.contourArea(contours[i]) < min_area_threshold
+                 or cv.contourArea(contours[i]) > max_area_threshold]
+    contours = [i for j, i in enumerate(contours) if j not in drop_list]
+
+    frames_contours = []
+    for cnt in contours:
+        epsilon = perimeter_threshold * cv.arcLength(cnt, True)
+        approx = cv.approxPolyDP(cnt, epsilon, True)
+        convexity = cv.isContourConvex(approx)
+        corners_count = len(approx)
+
+        if convexity and corners_count == 4:
+            frames_contours.append(approx.tolist())
+
+    if len(frames_contours) > 0:
+        return frames_contours
+    else:
+        return []
+
+
+def data_preprocessed(contours, frame_idx, df, row_index):
+
+    if contours != 0:
+        for i, v in enumerate(contours):
+            df.loc[row_index] = frame_idx, \
+                                v[0][0][0], v[0][0][1], v[1][0][0], v[1][0][1], \
+                                v[2][0][0], v[2][0][1], v[3][0][0], v[3][0][1]
+            row_index += 1
+
+    return row_index
+
+
+def data_cleaning(df, min_frames_quantity, file):
+    data = pd.read_csv(df)
+
+    frames_idx = []
+    j = 0
+    for idx in range(len(data.frame) - 1):
+        if data.frame[idx+1] - data.frame[idx] == 1:
+            j += 1
+        elif data.frame[idx+1] - data.frame[idx] == 0:
+            continue
+        else:
+            j = 0
+        frames_idx.append([data.frame[idx], j])
+
+    idx_counter = []
+    for item in frames_idx:
+        if item[1] >= min_frames_quantity:
+            idx_counter.append(item)
+
+    final_frames = []
+    for i in range(len(idx_counter)):
+        if idx_counter[i][1] == min_frames_quantity:
+            if i != 0:
+                final_frames.append([(idx_counter[i-1][0] + 1) - idx_counter[i-1][1],
+                                     idx_counter[i-1][0] + 1])
+        if i == len(idx_counter) - 1:
+            final_frames.append([(idx_counter[i - 1][0] + 1) - idx_counter[i - 1][1],
+                                 idx_counter[i - 1][0] + 2])
+
+    fragments = []
+    for fragment in final_frames:
+        file.write('Captured fragment: {}\n'.format(fragment))
+        fragments.append(np.arange(fragment[0], fragment[1] + 1))
+
+    idx_list = []
+    for fragment in fragments:
+        for idx in fragment:
+            idx_list.append(idx)
+
+    return idx_list
+
+
+def drawing_contours(csv, idx_frame, frame):
+    data = pd.read_csv(csv)
+
+    for i, _ in data.iterrows():
+        if data.frame[i] == idx_frame:
+            contour = [[[data.x1[i], data.y1[i]]], [[data.x2[i], data.y2[i]]],
+                       [[data.x3[i], data.y3[i]]], [[data.x4[i], data.y4[i]]]]
+            contour = np.array(contour)
+            cv.drawContours(frame, [contour], -1, (0, 255, 0), 2)
+
+
+def execution(video, img_path, video_path='Set path', preprocessing=True):
     if not video:
-        img = cv.imread(files_folder_path + img_name)
-        # lines = detect_lines(img)
-        detect_shapes(img)
+        img = cv.imread(img_path)
+        cnt = detect_shapes(img)
         cv.imshow('result', img)
-        # cv.imwrite(files_folder_path + 'single_line.png', lines)
         key = cv.waitKey(0)
         if key == 27:
             cv.destroyAllWindows()
     else:
-        capture = cv.VideoCapture(results_folder + 'Avengers.mkv')
+        capture = cv.VideoCapture(video_path)
         frame_width = int(capture.get(3))
         frame_height = int(capture.get(4))
         four_cc = cv.VideoWriter_fourcc(*'FMP4')  # XVID FMP4 X264
         frames_count = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
         fps = capture.get(cv.CAP_PROP_FPS)
-        out = cv.VideoWriter(results_folder + 'results/detect_quadrilaterals.avi',
+        out = cv.VideoWriter('/home/worker/Shape_Detector/results/draw_quadrilaterals.avi',
                              four_cc, fps, (frame_width, frame_height), True)
+        if preprocessing:
+            columns = ['frame', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4']
+            data = pd.DataFrame(columns=columns)
+            row_idx = 0
+
+            for i in range(frames_count):
+                _, frame = capture.read()
+                contours = data_collection(frame)
+                row_idx = data_preprocessed(contours, i, data, row_idx)
+            data.to_csv('data.csv', index=False)
+
+        with open('report.txt', 'w') as f:
+            f.write('Frames count: {}\n'.format(frames_count))
+            f.write('FPS amount: {}\n'.format(fps))
+            frames_ranges = data_cleaning('data.csv', 90, f)
 
         for i in range(frames_count):
             _, frame = capture.read()
-            detect_shapes(frame)
-            # cv.imshow('result', frame)
+            if i in frames_ranges:
+                drawing_contours('data.csv', i, frame)
+
+            # cv.imshow('frame', frame)
             out.write(frame)
 
             # key = cv.waitKey(1)
@@ -156,7 +274,9 @@ def build_model(video, img_name='Set name', results_folder='Set path'):
         out.release()
 
 
-folder = '/home/worker/Shape_Detector/'
-image_name = ''
+folder = '/home/worker/Shape_Detector/Avengers.mkv'
+image_name = 'frame434.png'
 
-build_model(True, image_name, folder)
+start_time = time.time()
+execution(True, image_name, folder)
+print("--- %s seconds ---" % (time.time() - start_time))
